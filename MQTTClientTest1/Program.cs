fe2233637f04a73eb9566743af19c3c61cf19063
k1601619 <k1601619@C-A4404-06>
2018-02-13 12:38:23 +0200

    Add project files.

---
+++ MQTTClientTest1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// including the M2Mqtt Library
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MQTTClientTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("start");
+            // Create Client instance
+            MqttClient myClient = new MqttClient("192.168.28.128");
+
+            // Register to message received
+            myClient.MqttMsgPublishReceived += client_recievedMessage;
+
+            string clientId = Guid.NewGuid().ToString();
+            myClient.Connect(clientId);
+
+            // Subscribe to topic
+            myClient.Subscribe(new String[] { "meas/#" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            // myClient.Subscribe(new String[] { "paine" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            // myClient.Subscribe(new String[] { "lämpö" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+            System.Console.ReadLine();
+        }
+
+       
+
+        static void client_recievedMessage(object sender, MqttMsgPublishEventArgs e)
+        {
+            // Handle message received
+            
+            var message = System.Text.Encoding.Default.GetString(e.Message);
+            
+            System.Console.WriteLine("Message received: " + message + " " + e.Topic);
+        }
+    }
+}
